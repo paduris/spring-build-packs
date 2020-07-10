743408e7d9631d9d3bb3404ee42b799626f3b095
spaduri <suresh.paduri@gmail.com>
2020-07-10 14:07:25 -0600

    Initial commit

---
+++ src/main/java/com/paduri/github/springbuildpacks/SpringBuildPacksApplication.java
@@ -0,0 +1,13 @@
+package com.paduri.github.springbuildpacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBuildPacksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBuildPacksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paduri/github/springbuildpacks/SpringBuildPacksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paduri.github.springbuildpacks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBuildPacksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
